4018393c4b7fed409c0cc0ed787db88f8c767b2c
QQBoxy <gs1458@gmail.com>
2018-07-25 09:44:54 +0800

    Add example 1

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//Example1 - argcargv
+
+//まノㄧΑ畐ㄏノ夹非ㄧΑ畐
+#include <iostream>
+
+//夹非ㄧΑ畐㏑丁
+using namespace std;
+
+//ㄧΑ糶猭
+int main(const int argc, char *argv[]) //莉眔场把计
+{
+	//场把计计
+	cout << "argc: " << argc << endl;
+
+	//场把计ず甧
+	cout << "rgv[0]" << argv[0] << endl;
+
+	//癳Command㏑: pause
+	system("pause");
+
+	//肚沧ゎ
+	return 0;
+}
